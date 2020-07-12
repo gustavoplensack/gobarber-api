c3bc6f8ae95ea1a121bc26ba7805f4a12b22a7e2
Gustavo <gustavoplensack@gmail.com>
2020-07-11 21:56:53 -0300

    connection to db and first migration

---
+++ src/database/index.ts
@@ -0,0 +1,8 @@
+import {createConnection} from 'typeorm'
+
+
+/**
+ * No credentials are required here, they are being imported
+ * from the ormconfig.json file
+ */
+createConnection()
---
+++ src/database/migrations/1594512020433-CreateAppointments.ts
@@ -0,0 +1,39 @@
+import {MigrationInterface, QueryRunner, Table} from 'typeorm';
+
+/**
+ * Implements the migration in a pair:
+ *  - Generates the changes;
+ *  - Reverts it;
+ */
+export class CreateAppointments1594512020433 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+          name:'appointments',
+          columns:[
+            {
+              name:'id',
+              type:'varchar',
+              isPrimary:true,
+              generationStrategy:'uuid'
+            },
+            {
+              name:'provider',
+              type:'varchar',
+              isNullable:false
+            },
+            {
+              name:'date',
+              type:'timestamp with time zone',
+              isNullable:false
+            }
+          ]
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+
+}
