2af3baa7e35686711050f78e40934f3772ebd34b
Gustavo <gustavoplensack@gmail.com>
2020-12-07 08:48:19 -0300

    Add test coverage for user creation service

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,42 @@
+import AppError from '@shared/error/AppError';
+
+import FakeUserRepository from '../repositories/fakes/FakeUserRepository';
+import CreateUserService from './CreateUserServices';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+
+    const createUSer = new CreateUserService(fakeUserRepository);
+
+    const newUserData = {
+      email: 'jose@dascouves.email.com',
+      name: 'jose',
+      password: 'couves',
+    };
+
+    const user = await createUSer.execute(newUserData);
+
+    expect(user).toHaveProperty('id');
+    expect(user.name).toBe(newUserData.name);
+    expect(user.email).toBe(newUserData.email);
+  });
+
+  it('should not be able to create an user with an already used email', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+
+    const createUSer = new CreateUserService(fakeUserRepository);
+
+    const newUserData = {
+      email: 'jose@dascouves.email.com',
+      name: 'jose',
+      password: 'couves',
+    };
+
+    await createUSer.execute(newUserData);
+
+    await expect(createUSer.execute(newUserData)).rejects.toBeInstanceOf(
+      AppError,
+    );
+  });
+});
