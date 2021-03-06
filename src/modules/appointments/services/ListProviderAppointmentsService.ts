61e492054230268fe56beddc24c082d810b62523
Gustavo <gustavoplensack@gmail.com>
2021-03-06 20:50:06 -0300

    Fix cache serialization

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/AppointmentsModel';
 import IAppointmentsRepository from '../repositories/IAppointmensRepository';
 
@@ -44,7 +45,7 @@ export default class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
